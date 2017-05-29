d03b9409cb02c549b852c5321a89fd0dfa7b4dd0
mcDandy <danda.martinek@gmail.com>
2017-05-29 16:30:53 +0200

    added placeholder texture

--- src/main/java/cz/mcDandy/winksmod/ExampleMod.java
+++ src/main/java/cz/mcDandy/winksmod/ExampleMod.java
@@ -10,7 +10,7 @@ import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 @Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
 public class ExampleMod
 {
-    public static final String MODID = "examplemod";
+    public static final String MODID = "winksmod";
     public static final String VERSION = "1.0";
     
     @EventHandler
