c1959ff560e576a60cba596f9a4316ce3f5c7de4
mcDandy <danda.martinek@gmail.com>
2017-09-26 12:19:32 +0200

    reformat

--- src/main/java/cz/mcDandy/winksmod/Event.java
+++ src/main/java/cz/mcDandy/winksmod/Event.java
@@ -7,35 +7,40 @@ import net.minecraft.util.EnumHand;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
 
-public class Event{
-EntityPlayer player;
-EnumHand hand;
-@SubscribeEvent
- public void onPlayerTick(TickEvent.PlayerTickEvent event) {
-	//ItemStack itemstack = player.getHeldItem(hand);
-	//if(itemstack == ModItems.wings){}
-}
- 
- //Called when the client ticks. 
- @SubscribeEvent
- public void onClientTick(TickEvent.ClientTickEvent event) {
+public class Event {
+	EntityPlayer player;
+	EnumHand hand;
+	int FiaryLevel;
+	NBTTagCompound nbt = new NBTTagCompound();
+	@SubscribeEvent
+	public void onPlayerTick(TickEvent.PlayerTickEvent event) {
+		// ItemStack itemstack = player.getHeldItem(hand);
+		// if(itemstack == ModItems.wings){}
+	}
 
-}
- 
- //Called when the server ticks. Usually 20 ticks a second. 
- @SubscribeEvent
- public void onServerTick(TickEvent.ServerTickEvent event) {
+	// Called when the client ticks.
+	@SubscribeEvent
+	public void onClientTick(TickEvent.ClientTickEvent event) {
 
-}
- 
- //Called when a new frame is displayed (See fps) 
- @SubscribeEvent
- public void onRenderTick(TickEvent.RenderTickEvent event) {
+	}
 
-}
- 
- //Called when the world ticks
- @SubscribeEvent
- public void onWorldTick(TickEvent.WorldTickEvent event) {
+	// Called when the server ticks. Usually 20 ticks a second.
+	@SubscribeEvent
+	public void onServerTick(TickEvent.ServerTickEvent event) {
 
-}}
+	}
+
+	// Called when a new frame is displayed (See fps)
+	@SubscribeEvent
+	public void onRenderTick(TickEvent.RenderTickEvent event) {
+
+	}
+
+	// Called when the world ticks
+	@SubscribeEvent
+	public void onWorldTick(TickEvent.WorldTickEvent event) {
+
+	}
+
+	}
+}
