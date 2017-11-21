b43e240e0d8e4d2e051cae322a646c607449be33
mcDandy <danda.martinek@gmail.com>
2017-11-21 08:53:10 +0100

    First try

---
+++ src/main/java/cz/mcDandy/winksmod/Entities/FiaryGeneric.java
@@ -0,0 +1,29 @@
+package cz.mcDandy.winksmod.Entities;
+
+
+import net.minecraft.entity.ai.EntityAIBase;
+import net.minecraft.entity.ai.EntityAIDoorInteract;
+import net.minecraft.entity.ai.EntityAILookIdle;
+import net.minecraft.entity.ai.EntityAISwimming;
+import net.minecraft.entity.ai.EntityAIWander;
+import net.minecraft.entity.monster.EntityMob;
+import net.minecraft.world.World;
+
+public class FiaryGeneric extends EntityMob
+{
+
+	public FiaryGeneric(World worldIn) {
+		super(worldIn);
+		// TODO Auto-generated constructor stub
+	}
+	@Override
+	protected void initEntityAI()
+	{   
+		tasks.addTask(0, new EntityAILookIdle(this));
+		tasks.addTask(0, new EntityAISwimming(this));
+		tasks.addTask(0, new EntityAIWander(this, interpTargetPitch, 10));
+		
+	//	tasks.addTask(0, new EntityAIDoorInteract(this));
+	}
+	
+}
--- src/main/java/cz/mcDandy/winksmod/Main.java
+++ src/main/java/cz/mcDandy/winksmod/Main.java
@@ -2,13 +2,16 @@ package cz.mcDandy.winksmod;
 
 import cz.mcDandy.winksmod.register.Crafting;
 import cz.mcDandy.winksmod.register.Register;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.entity.RenderManager;
 import net.minecraft.init.Blocks;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
-
+import cz.mcDandy.winksmod.Entities.FiaryGeneric;
 @Mod(modid = Main.MODID, version = Main.VERSION)
 public class Main {
 	public static final String MODID = "winksmod";
@@ -23,6 +26,8 @@ public class Main {
 
 	@EventHandler
 	public void init(FMLInitializationEvent event) {
+		RenderManager rm = Minecraft.getMinecraft().getRenderManager();
+		RenderingRegistry.registerEntityRenderingHandler(entityClass, renderFactory);
 		Crafting.Register(event);
 		MinecraftForge.EVENT_BUS.register(new Event());
 		// some example code
