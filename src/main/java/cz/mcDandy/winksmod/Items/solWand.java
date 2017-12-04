d3367d7363c05b5b4f49b60765c0baac09f8f041
mcDandy <danda.martinek@gmail.com>
2017-12-04 15:42:55 +0100

    reformat

--- src/main/java/cz/mcDandy/winksmod/Items/solWand.java
+++ src/main/java/cz/mcDandy/winksmod/Items/solWand.java
@@ -1,12 +1,9 @@
 package cz.mcDandy.winksmod.Items;
 
 import cz.mcDandy.winksmod.Main;
-import cz.mcDandy.winksmod.register.ModItems;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.entity.projectile.EntityFireball;
 import net.minecraft.entity.projectile.EntityLargeFireball;
-import net.minecraft.init.Items;
 import net.minecraft.item.EnumAction;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
@@ -15,21 +12,19 @@ import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumHand;
 import net.minecraft.world.World;
 
-
 public class solWand extends Item {
-//	ItemStack itemstack = new ItemStack(ModItems.solwand);
-
+	// ItemStack itemstack = new ItemStack(ModItems.solwand);
 
 	public solWand(String unlocalizedName, CreativeTabs tab, int stack) {
 		super();
-this.setRegistryName(Main.MODID,unlocalizedName);
-this.setUnlocalizedName(unlocalizedName);
-this.setCreativeTab(tab);
-this.maxStackSize=1;
-this.setMaxDamage(100);
-		//showDurabilityBar(null);
+		this.setRegistryName(Main.MODID, unlocalizedName);
+		this.setUnlocalizedName(unlocalizedName);
+		this.setCreativeTab(tab);
+		this.maxStackSize = 1;
+		this.setMaxDamage(100);
+		showDurabilityBar(null);
 		isDamageable();
-	//	getDurabilityForDisplay();
+		// getDurabilityForDisplay();
 		// TODO Auto-generated constructor stub
 	}
 
@@ -43,48 +38,36 @@ this.setMaxDamage(100);
 		return true;
 	}
 
-//	public EnumAction getItemUseAction(ItemStack stack) {
-//		return EnumAction.BOW;
-//	}
-
-	/**
-	 * Called when the equipped item is right clicked.
-	 */
-	@Override
-	  public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn)
-	    {
-	        ItemStack itemstack = playerIn.getHeldItem(handIn);
-
-	        if (worldIn.isRemote)
-	        {
-	            return new ActionResult<ItemStack>(EnumActionResult.PASS, itemstack);
-	        }
-	        else
-	        {
-
-
-	                if (itemstack.getMaxDamage() - itemstack.getMetadata() > 10)
-	                {
-	                    itemstack.damageItem(10, playerIn);
-	                    EntityLargeFireball fireball = new EntityLargeFireball(worldIn, playerIn, 10, 10, 10);
-
-	        			fireball.posX = playerIn.posX;
-	        			fireball.posY = playerIn.posY + playerIn.eyeHeight;
-	        			fireball.posZ = playerIn.posZ;
-	        			fireball.accelerationX = playerIn.getLookVec().xCoord;
-	        			fireball.accelerationY = playerIn.getLookVec().yCoord;
-	        			fireball.accelerationZ = playerIn.getLookVec().zCoord;
-	        			worldIn.spawnEntity(fireball);
-	                    
-
-	                    return super.onItemRightClick(worldIn, playerIn, handIn);
-	                }
-	           
-
-	          
-	            return new ActionResult<ItemStack>(EnumActionResult.PASS, itemstack);
-	            }
-	        }
-
-
+	public EnumAction getItemUseAction(ItemStack stack) {
+		return EnumAction.BLOCK;
 	}
+
+	@Override
+	public ActionResult<ItemStack> onItemRightClick(World worldIn, EntityPlayer playerIn, EnumHand handIn) {
+		// super.onItemRightClick(worldIn, playerIn, playerIn.getActiveHand());
+		ItemStack itemstack = playerIn.getHeldItem(handIn);
+
+		if (worldIn.isRemote) {
+			// return ActionResult<ItemStack>();
+		} else {
+			if (itemstack.getMaxDamage() - itemstack.getMetadata() > 10) {
+				itemstack.damageItem(10, playerIn);
+				EntityLargeFireball fireball = new EntityLargeFireball(worldIn, playerIn, 10, 10, 10);
+
+				fireball.posX = playerIn.posX;
+				fireball.posY = playerIn.posY + playerIn.eyeHeight;
+				fireball.posZ = playerIn.posZ;
+				fireball.accelerationX = playerIn.getLookVec().xCoord;
+				fireball.accelerationY = playerIn.getLookVec().yCoord;
+				fireball.accelerationZ = playerIn.getLookVec().zCoord;
+				worldIn.spawnEntity(fireball);
+
+				return new ActionResult(EnumActionResult.SUCCESS, playerIn.getHeldItem(handIn));
+			}
+
+			return new ActionResult(EnumActionResult.SUCCESS, playerIn.getHeldItem(handIn));
+		}
+		return new ActionResult(EnumActionResult.PASS, playerIn.getHeldItem(handIn));
+	}
+
+}
--- src/main/java/cz/mcDandy/winksmod/register/ModItems.java
+++ src/main/java/cz/mcDandy/winksmod/register/ModItems.java
@@ -1,17 +1,13 @@
 package cz.mcDandy.winksmod.register;
 
 import cz.mcDandy.winksmod.Items.*;
-import net.minecraft.block.Block;
-import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemStack;
-import net.minecraft.potion.PotionType;
 
 public class ModItems {
 
 	public static Item fpowder = new fpowder("fpowder", CreativeTabs.MATERIALS);
-	public static Item solwand = new solwand("solwand", CreativeTabs.COMBAT, 1);
+	public static Item solwand = new solWand("solwand", CreativeTabs.COMBAT, 1);
 	public static Item wings = new wings("wings",CreativeTabs.TRANSPORTATION,1);
 	//public static Item MysticPotion = new mPotion("MysticPotion",CreativeTabs.BREWING,1);
 	public static Item magicdiamond = new magicdiamond("magicdiamond",CreativeTabs.MISC,1);
