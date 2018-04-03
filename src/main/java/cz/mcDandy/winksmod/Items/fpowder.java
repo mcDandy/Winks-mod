e7ab7ef1614bc36f01b9323d98fdcef13c548285
mcDandy <danda.martinek@gmail.com>
2018-04-03 14:54:43 +0200

    Fixed crash

--- src/main/java/cz/mcDandy/winksmod/Items/fpowder.java
+++ src/main/java/cz/mcDandy/winksmod/Items/fpowder.java
@@ -9,17 +9,22 @@ import net.minecraft.world.World;
 public class fpowder extends ItemFoodBase {
 
 	public fpowder(String unlocalizedName, CreativeTabs tab) {
-		super(unlocalizedName, tab,64,0,true);
+		super(unlocalizedName, tab, 64, 0, true);
 		// TODO Auto-generated constructor stub
 	}
+
 	@Override
-	public void onFoodEaten(ItemStack stack, World worldIn, EntityPlayer player) 
-	{
-		for(PotionEffect eff:player.getActivePotionEffects()) {
-			if(eff.getPotion().isBadEffect()) 
-			{
-				player.removeActivePotionEffect(eff.getPotion());
+	public void onFoodEaten(ItemStack stack, World worldIn, EntityPlayer player) {
+		PotionEffect[] pf = new PotionEffect[player.getActivePotionEffects().size()];
+		int a = 0;
+		for (PotionEffect eff : player.getActivePotionEffects()) {
+			if (eff.getPotion().isBadEffect()) {
+				pf[a] = eff;
+				a++;
 			}
-			}
+		}
+		for (int s = 0; s < a; s++) {
+			player.removeActivePotionEffect(pf[s].getPotion());
+		}
 	}
 }
