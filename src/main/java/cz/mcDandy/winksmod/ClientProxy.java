2d4b466b82f79d79921b949add643b91ce2e3084
mcDandy <danda.martinek@gmail.com>
2018-04-03 13:55:31 +0200

    cleanup

--- src/main/java/cz/mcDandy/winksmod/ClientProxy.java
+++ src/main/java/cz/mcDandy/winksmod/ClientProxy.java
@@ -1,12 +1,6 @@
 package cz.mcDandy.winksmod;
 
 import cz.mcDandy.winksmod.Entities.ModEntities;
-import cz.mcDandy.winksmod.register.ModBlocks;
-import cz.mcDandy.winksmod.register.ModItems;
-import net.minecraft.block.Block;
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
-import net.minecraft.item.Item;
-import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
--- src/main/java/cz/mcDandy/winksmod/Event.java
+++ src/main/java/cz/mcDandy/winksmod/Event.java
@@ -1,18 +1,10 @@
 package cz.mcDandy.winksmod;
 
-import cz.mcDandy.winksmod.register.ModBlocks;
 import cz.mcDandy.winksmod.register.ModItems;
-import net.minecraft.block.Block;
-import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.item.Item;
-import net.minecraft.item.ItemBlock;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.EnumHand;
-import net.minecraftforge.client.event.ModelRegistryEvent;
-import net.minecraftforge.client.model.ModelLoader;
-import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.event.entity.living.LivingEvent.LivingUpdateEvent;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
