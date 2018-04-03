5dec316471d35455197dde6fa23b9feb78fe3f9a
mcDandy <danda.martinek@gmail.com>
2018-04-03 12:23:22 +0200

    fiary powder is edible
    when eaten it will remove negative status effect
    
    (not tested yet)

--- src/main/java/cz/mcDandy/winksmod/Items/fpowder.java
+++ src/main/java/cz/mcDandy/winksmod/Items/fpowder.java
@@ -1,11 +1,25 @@
 package cz.mcDandy.winksmod.Items;
 
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.PotionEffect;
+import net.minecraft.world.World;
 
-public class fpowder extends ItemBase {
+public class fpowder extends ItemFoodBase {
 
 	public fpowder(String unlocalizedName, CreativeTabs tab) {
-		super(unlocalizedName, tab);
+		super(unlocalizedName, tab,64,0,true);
 		// TODO Auto-generated constructor stub
+	}
+	@Override
+	public void onFoodEaten(ItemStack stack, World worldIn, EntityPlayer player) 
+	{
+		for(PotionEffect eff:player.getActivePotionEffects()) {
+			if(eff.getPotion().isBadEffect()) 
+			{
+				player.removeActivePotionEffect(eff.getPotion());
+			}
+			}
 	}
 }
