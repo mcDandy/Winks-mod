e0dd2e671c0c08961421ebd09d3e56bed044dc18
mcDandy <danda.martinek@gmail.com>
2018-04-04 14:16:46 +0200

    blockcharger fixes

--- src/main/java/cz/mcDandy/winksmod/register/ModBlocks.java
+++ src/main/java/cz/mcDandy/winksmod/register/ModBlocks.java
@@ -8,5 +8,6 @@ import net.minecraft.creativetab.CreativeTabs;
 public class ModBlocks {
 
 	public static Block fp_block = new fp_Block("fp_block", Material.SAND, ModTabs.fmod);
-	public static Block[] BLOCKS = new Block[] { fp_block };
+	public static Block BlockCharger = new fp_Block("BlockCharger", Material.CIRCUITS, ModTabs.fmod);
+	public static Block[] BLOCKS = new Block[] { fp_block,BlockCharger};
 }
