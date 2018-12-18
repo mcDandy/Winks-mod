fe3d06937315e01ed586197dcf288d658071d6fc
mcDandy <danda.martinek@gmail.com>
2018-12-18 10:18:13 +0100

    Fixed compile errors in code copied from mccreator (www.mccreator.net)

--- src/main/java/cz/mcDandy/winksmod/GUI/TpWandGui.java
+++ src/main/java/cz/mcDandy/winksmod/GUI/TpWandGui.java
@@ -21,220 +21,183 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLServerStartingEvent;
 
+public class TpWandGui extends GuiScreen {
 
-public class TpWandGui {
+	int x, y, z;
+	EntityPlayer entity;
+	GuiTextField Xpos;
+	GuiTextField Ypos;
+	GuiTextField Zpos;
+	private int guiLeft;
+	private int guiTop;
+	private int xSize;
+	private int ySize;
+	private HashMap guiinventory;
 
-	//public static Object instance;
+	public TpWandGui(World world, int x, int y, int z, EntityPlayer entity) {
+		super();
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		this.entity = entity;
+		this.xSize = 176;
+		this.ySize = 166;
+	}
 
+	private static final ResourceLocation texture = new ResourceLocation("tpwandgui.png");
 
-	public static int GUIID = 1;
-	public static HashMap guiinventory = new HashMap();
+	@Override
+	public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+		this.drawDefaultBackground();
+		super.drawScreen(mouseX, mouseY, partialTicks);
+		// this.renderHoveredToolTip(mouseX, mouseY);
+	}
 
-	public static class GuiContainerMod extends Container {
-
-		World world;
-		EntityPlayer entity;
-		int x, y, z;
-
-		public GuiContainerMod(World world, int x, int y, int z, EntityPlayer player) {
-			this.world = world;
-			this.entity = player;
-			this.x = x;
-			this.y = y;
-			this.z = z;
-		}
-
-		@Override
-		public boolean canInteractWith(EntityPlayer player) {
-			return true;
-		}
-
-		public void onContainerClosed(EntityPlayer playerIn) {
-			super.onContainerClosed(playerIn);
+	@Override
+	protected void mouseClicked(int mouseX, int mouseY, int mouseButton) {
+		try {
+			super.mouseClicked(mouseX, mouseY, mouseButton);
+			Xpos.mouseClicked(mouseX - guiLeft, mouseY - guiTop, mouseButton);
+			Ypos.mouseClicked(mouseX - guiLeft, mouseY - guiTop, mouseButton);
+			Zpos.mouseClicked(mouseX - guiLeft, mouseY - guiTop, mouseButton);
+		} catch (Exception ignored) {
 		}
 	}
 
-	public static class GuiWindow extends GuiScreen {
+	@Override
+	public void updateScreen() {
+		super.updateScreen();
+		Xpos.updateCursorCounter();
+		Ypos.updateCursorCounter();
+		Zpos.updateCursorCounter();
+	}
 
-		int x, y, z;
-		EntityPlayer entity;
-		GuiTextField Xpos;
-		GuiTextField Ypos;
-		GuiTextField Zpos;
-		private int guiLeft;
-		private int guiTop;
-		private int xSize;
-		private int ySize;
-
-		public GuiWindow(World world, int x, int y, int z, EntityPlayer entity) {
-			super();
-			this.x = x;
-			this.y = y;
-			this.z = z;
-			this.entity = entity;
-			this.xSize = 176;
-			this.ySize = 166;
+	@Override
+	protected void keyTyped(char typedChar, int keyCode) {
+		try {
+			super.keyTyped(typedChar, keyCode);
+			Xpos.textboxKeyTyped(typedChar, keyCode);
+			Ypos.textboxKeyTyped(typedChar, keyCode);
+			Zpos.textboxKeyTyped(typedChar, keyCode);
+		} catch (Exception ignored) {
 		}
+	}
 
-		private static final ResourceLocation texture = new ResourceLocation("tpwandgui.png");
+	@Override
+	public void onGuiClosed() {
+		super.onGuiClosed();
+		// Keyboard.enableRepeatEvents(false);
+	}
 
-		@Override
-		public void drawScreen(int mouseX, int mouseY, float partialTicks) {
-			this.drawDefaultBackground();
-			super.drawScreen(mouseX, mouseY, partialTicks);
-		//	this.renderHoveredToolTip(mouseX, mouseY);
-		}
+	@Override
+	public void initGui() {
+		super.initGui();
+		this.guiLeft = (this.width - 176) / 2;
+		this.guiTop = (this.height - 166) / 2;
+		// Keyboard.enableRepeatEvents(true);
+		this.buttonList.clear();
+		this.buttonList.add(new GuiButton(0, this.guiLeft + 47, this.guiTop + 126, 86, 20, "Teleport"));
+		Xpos = new GuiTextField(0, this.fontRenderer, 38, 19, 120, 20);
+		guiinventory.put("text:Xpos", Xpos);
+		Xpos.setMaxStringLength(32767);
+		Xpos.setFocused(true);
+		Xpos.setText("");
+		Ypos = new GuiTextField(1, this.fontRenderer, 38, 45, 120, 20);
+		guiinventory.put("text:Ypos", Ypos);
+		Ypos.setMaxStringLength(32767);
+		Ypos.setFocused(true);
+		Ypos.setText("");
+		Zpos = new GuiTextField(2, this.fontRenderer, 39, 70, 120, 20);
+		guiinventory.put("text:Zpos", Zpos);
+		Zpos.setMaxStringLength(32767);
+		Zpos.setFocused(true);
+		Zpos.setText("");
 
-		@Override
-		protected void drawGuiContainerBackgroundLayer(float par1, int par2, int par3) {
-			GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-			this.drawDefaultBackground();
-			this.mc.renderEngine.bindTexture(texture);
-			int k = (this.width - this.xSize) / 2;
-			int l = (this.height - this.ySize) / 2;
-			this.drawTexturedModalRect(k, l, 0, 0, this.xSize, this.ySize);
-			zLevel = 100.0F;
-		}
+		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+		this.drawDefaultBackground();
+		this.mc.renderEngine.bindTexture(texture);
+		int k = (this.width - this.xSize) / 2;
+		int l = (this.height - this.ySize) / 2;
+		this.drawTexturedModalRect(k, l, 0, 0, this.xSize, this.ySize);
+		zLevel = 100.0F;
 
-		@Override
-		protected void mouseClicked(int mouseX, int mouseY, int mouseButton) {
-			try {
-				super.mouseClicked(mouseX, mouseY, mouseButton);
-				Xpos.mouseClicked(mouseX - guiLeft, mouseY - guiTop, mouseButton);
-				Ypos.mouseClicked(mouseX - guiLeft, mouseY - guiTop, mouseButton);
-				Zpos.mouseClicked(mouseX - guiLeft, mouseY - guiTop, mouseButton);
-			} catch (Exception ignored) {
+		Xpos.drawTextBox();
+		Ypos.drawTextBox();
+		Zpos.drawTextBox();
+		this.fontRenderer.drawString("X:", 18, 20, -16777216);
+		this.fontRenderer.drawString("Y:", 19, 51, -16777216);
+		this.fontRenderer.drawString("Z:", 19, 75, -16777216);
+
+	}
+
+	@Override
+	protected void actionPerformed(GuiButton button) {
+		MinecraftServer server = FMLCommonHandler.instance().getMinecraftServerInstance();
+		World world = server.getWorld(entity.dimension);
+		if (button.id == 0) {
+			{
+				if (entity instanceof EntityLivingBase)
+					((EntityLivingBase) entity).setPositionAndUpdate((int) new Object() {
+
+						int convert(String s) {
+							try {
+								return Integer.parseInt(s.trim());
+							} catch (Exception e) {
+							}
+							return 0;
+						}
+					}.convert((new Object() {
+
+						public String getText() {
+							GuiTextField textField = (GuiTextField) guiinventory.get("text:Xpos");
+							if (textField != null) {
+								return textField.getText();
+							}
+							return "";
+						}
+					}.getText())), (int) new Object() {
+
+						int convert(String s) {
+							try {
+								return Integer.parseInt(s.trim());
+							} catch (Exception e) {
+							}
+							return 0;
+						}
+					}.convert((new Object() {
+
+						public String getText() {
+							GuiTextField textField = (GuiTextField) guiinventory.get("text:Ypos");
+							if (textField != null) {
+								return textField.getText();
+							}
+							return "";
+						}
+					}.getText())), (int) new Object() {
+
+						int convert(String s) {
+							try {
+								return Integer.parseInt(s.trim());
+							} catch (Exception e) {
+							}
+							return 0;
+						}
+					}.convert((new Object() {
+
+						public String getText() {
+							GuiTextField textField = (GuiTextField) guiinventory.get("text:Zpos");
+							if (textField != null) {
+								return textField.getText();
+							}
+							return "";
+						}
+					}.getText())));
 			}
 		}
+	}
 
-		@Override
-		public void updateScreen() {
-			super.updateScreen();
-			Xpos.updateCursorCounter();
-			Ypos.updateCursorCounter();
-			Zpos.updateCursorCounter();
-		}
-
-		@Override
-		protected void keyTyped(char typedChar, int keyCode) {
-			try {
-				super.keyTyped(typedChar, keyCode);
-				Xpos.textboxKeyTyped(typedChar, keyCode);
-				Ypos.textboxKeyTyped(typedChar, keyCode);
-				Zpos.textboxKeyTyped(typedChar, keyCode);
-			} catch (Exception ignored) {
-			}
-		}
-
-		@Override
-		protected void drawGuiContainerForegroundLayer(int par1, int par2) {
-			Xpos.drawTextBox();
-			Ypos.drawTextBox();
-			Zpos.drawTextBox();
-			this.fontRenderer.drawString("X:", 18, 20, -16777216);
-			this.fontRenderer.drawString("Y:", 19, 51, -16777216);
-			this.fontRenderer.drawString("Z:", 19, 75, -16777216);
-		}
-
-		@Override
-		public void onGuiClosed() {
-			super.onGuiClosed();
-		//	Keyboard.enableRepeatEvents(false);
-		}
-
-		@Override
-		public void initGui() {
-			super.initGui();
-			this.guiLeft = (this.width - 176) / 2;
-			this.guiTop = (this.height - 166) / 2;
-	//		Keyboard.enableRepeatEvents(true);
-			this.buttonList.clear();
-			this.buttonList.add(new GuiButton(0, this.guiLeft + 47, this.guiTop + 126, 86, 20, "Teleport"));
-			Xpos = new GuiTextField(0, this.fontRenderer, 38, 19, 120, 20);
-			guiinventory.put("text:Xpos", Xpos);
-			Xpos.setMaxStringLength(32767);
-			Xpos.setFocused(true);
-			Xpos.setText("");
-			Ypos = new GuiTextField(1, this.fontRenderer, 38, 45, 120, 20);
-			guiinventory.put("text:Ypos", Ypos);
-			Ypos.setMaxStringLength(32767);
-			Ypos.setFocused(true);
-			Ypos.setText("");
-			Zpos = new GuiTextField(2, this.fontRenderer, 39, 70, 120, 20);
-			guiinventory.put("text:Zpos", Zpos);
-			Zpos.setMaxStringLength(32767);
-			Zpos.setFocused(true);
-			Zpos.setText("");
-		}
-
-		@Override
-		protected void actionPerformed(GuiButton button) {
-			MinecraftServer server = FMLCommonHandler.instance().getMinecraftServerInstance();
-			World world = server.getWorld(entity.dimension);
-			if (button.id == 0) {
-				{
-					if (entity instanceof EntityLivingBase)
-						((EntityLivingBase) entity).setPositionAndUpdate((int) new Object() {
-
-							int convert(String s) {
-								try {
-									return Integer.parseInt(s.trim());
-								} catch (Exception e) {
-								}
-								return 0;
-							}
-						}.convert((new Object() {
-
-							public String getText() {
-								GuiTextField textField = (GuiTextField) guiinventory.get("text:Xpos");
-								if (textField != null) {
-									return textField.getText();
-								}
-								return "";
-							}
-						}.getText())), (int) new Object() {
-
-							int convert(String s) {
-								try {
-									return Integer.parseInt(s.trim());
-								} catch (Exception e) {
-								}
-								return 0;
-							}
-						}.convert((new Object() {
-
-							public String getText() {
-								GuiTextField textField = (GuiTextField) guiinventory.get("text:Ypos");
-								if (textField != null) {
-									return textField.getText();
-								}
-								return "";
-							}
-						}.getText())), (int) new Object() {
-
-							int convert(String s) {
-								try {
-									return Integer.parseInt(s.trim());
-								} catch (Exception e) {
-								}
-								return 0;
-							}
-						}.convert((new Object() {
-
-							public String getText() {
-								GuiTextField textField = (GuiTextField) guiinventory.get("text:Zpos");
-								if (textField != null) {
-									return textField.getText();
-								}
-								return "";
-							}
-						}.getText())));
-				}
+	@Override
+	public boolean doesGuiPauseGame() {
+		return false;
+	}
 }
-			}
-		}
-
-		@Override
-		public boolean doesGuiPauseGame() {
-			return false;
-		}
