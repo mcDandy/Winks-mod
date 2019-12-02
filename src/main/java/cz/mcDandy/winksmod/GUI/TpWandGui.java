c32ebc08e170a331072e080b3908c29481afbff2
mcDandy <danda.martinek@gmail.com>
2019-12-02 18:09:17 +0100

    conflict

--- src/main/java/cz/mcDandy/winksmod/GUI/TpWandGui.java
+++ src/main/java/cz/mcDandy/winksmod/GUI/TpWandGui.java
@@ -1,6 +1,11 @@
 package cz.mcDandy.winksmod.GUI;
 
+<<<<<<< HEAD
 import org.lwjgl.input.Keyboard;
+=======
+import java.util.logging.Level;
+
+>>>>>>> master
 import org.lwjgl.opengl.GL11;
 
 import cz.mcDandy.winksmod.Main;
@@ -9,10 +14,9 @@ import net.minecraft.client.gui.GuiScreen;
 import net.minecraft.client.gui.GuiTextField;
 import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.server.MinecraftServer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
-import net.minecraftforge.fml.common.FMLCommonHandler;
 
 public class TpWandGui extends GuiScreen {
 
@@ -25,13 +29,21 @@ public class TpWandGui extends GuiScreen {
 	private int guiTop;
 	private int xSize;
 	private int ySize;
+	GuiButton tp;
 
-	public TpWandGui(World world, EntityPlayer entity) {
+	public TpWandGui(World world, EntityPlayer playerIn) {
 		super();
+<<<<<<< HEAD
 		this.x = entity.posX;
 		this.y = entity.posY;
 		this.z = entity.posZ;
 	  	this.entity = entity;
+=======
+		// this.x = entity.posX;
+		// this.y = entity.posY;
+		// this.z = entity.posZ;
+		this.entity = playerIn;
+>>>>>>> master
 		this.xSize = 176;
 		this.ySize = 166;
 	}
@@ -62,15 +74,21 @@ public class TpWandGui extends GuiScreen {
 		Zpos.setFocused(true);
 		Zpos.setText("");
 
+<<<<<<< HEAD
 	}
 	@Override
 	public void drawScreen(int mouseX, int mouseY, float partialTicks) {
 		
+=======
+	@Override
+	public void drawScreen(int mouseX, int mouseY, float partialTicks) {
+>>>>>>> master
 		GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
 		this.drawDefaultBackground();
 	//	this.mc.renderEngine.bindTexture(texture);
 		int k = (this.width - this.xSize) / 2;
 		int l = (this.height - this.ySize) / 2;
+<<<<<<< HEAD
 	//	this.drawTexturedModalRect(k, l, 0, 0, this.xSize, this.ySize);
 		zLevel = 100.0F;
 
@@ -81,16 +99,27 @@ public class TpWandGui extends GuiScreen {
 		this.fontRenderer.drawString("Y:", guiLeft+19, guiTop+51, -16777216);
 		this.fontRenderer.drawString("Z:", guiLeft+19, guiTop+75, -16777216);
 		 super.drawScreen(mouseX, mouseY, partialTicks);
+=======
+		this.drawTexturedModalRect(k, l, 0, 0, this.xSize, this.ySize);
+		Xpos.drawTextBox();
+		Ypos.drawTextBox();
+		Zpos.drawTextBox();
+		this.buttonList.get(0).drawButton(mc, mouseX, mouseY, partialTicks);
+		this.fontRenderer.drawString("X:", k + 19, l + 25, -16777216);
+		this.fontRenderer.drawString("Y:", k + 19, l + 50, -16777216);
+		this.fontRenderer.drawString("Z:", k + 19, l + 75, -16777216);
+>>>>>>> master
 	}
 
 	@Override
 	protected void mouseClicked(int mouseX, int mouseY, int mouseButton) {
 		try {
 			super.mouseClicked(mouseX, mouseY, mouseButton);
-			Xpos.mouseClicked(mouseX - guiLeft, mouseY - guiTop, mouseButton);
-			Ypos.mouseClicked(mouseX - guiLeft, mouseY - guiTop, mouseButton);
-			Zpos.mouseClicked(mouseX - guiLeft, mouseY - guiTop, mouseButton);
+			Xpos.mouseClicked(mouseX, mouseY, mouseButton);
+			Ypos.mouseClicked(mouseX, mouseY, mouseButton);
+			Zpos.mouseClicked(mouseX, mouseY, mouseButton);
 		} catch (Exception ignored) {
+			Main.logger.log(Level.WARNING, ignored.toString());
 		}
 	}
 
@@ -106,10 +135,14 @@ public class TpWandGui extends GuiScreen {
 	protected void keyTyped(char typedChar, int keyCode) {
 		try {
 			super.keyTyped(typedChar, keyCode);
-			Xpos.textboxKeyTyped(typedChar, keyCode);
-			Ypos.textboxKeyTyped(typedChar, keyCode);
-			Zpos.textboxKeyTyped(typedChar, keyCode);
+			if(Character.isDigit(typedChar)||typedChar=='\b'||keyCode==127||keyCode==37||keyCode==39)
+			{
+				Xpos.textboxKeyTyped(typedChar, keyCode);
+				Ypos.textboxKeyTyped(typedChar, keyCode);
+				Zpos.textboxKeyTyped(typedChar, keyCode);
+			}
 		} catch (Exception ignored) {
+			Main.logger.log(Level.SEVERE, ignored.toString());
 		}
 	}
 
@@ -119,19 +152,43 @@ public class TpWandGui extends GuiScreen {
 		 Keyboard.enableRepeatEvents(false);
 	}
 
+<<<<<<< HEAD
 
+=======
+	@Override
+	public void initGui() {
+		super.initGui();
+		this.guiLeft = (this.width - 176) / 2;
+		this.guiTop = (this.height - 166) / 2;
+		this.buttonList.add(new GuiButton(0, this.guiLeft + 47, this.guiTop + 126, 86, 20, "Teleport"));
+
+		Xpos = new GuiTextField(0, this.fontRenderer, this.guiLeft + 38, this.guiTop + 19, 120, 20);
+		Xpos.setMaxStringLength(32767);
+		Xpos.setFocused(true);
+		Xpos.setText("");
+
+		Ypos = new GuiTextField(1, this.fontRenderer, this.guiLeft + 38, this.guiTop + 45, 120, 20);
+		Ypos.setMaxStringLength(32767);
+		Ypos.setFocused(true);
+		Ypos.setText("");
+
+		Zpos = new GuiTextField(2, this.fontRenderer, this.guiLeft + 39, this.guiTop + 70, 120, 20);
+		Zpos.setMaxStringLength(32767);
+		Zpos.setFocused(true);
+		Zpos.setText("");
+
+	}
+>>>>>>> master
 
 	@Override
 	protected void actionPerformed(GuiButton button) {
-		MinecraftServer server = FMLCommonHandler.instance().getMinecraftServerInstance();
-		World world = server.getWorld(entity.dimension);
-		if (button.id == 0) {
-
-			if (entity instanceof EntityLivingBase)
-
-				entity.setPositionAndUpdate(Integer.parseInt(Xpos.getText().trim()),
-						Integer.parseInt(Ypos.getText().trim()), Integer.parseInt(Zpos.getText().trim()));
-
+		// Main.logger.log(Level.INFO, "Cliend clicked on button: " + button.id);
+		if (button.id == this.buttonList.get(0).id) {
+			double X = Double.parseDouble(Xpos.getText());
+			double Y = Double.parseDouble(Ypos.getText());
+			double Z = Double.parseDouble(Zpos.getText());
+			Main.logger.log(Level.INFO, "Cliend clicked on Teleport button.");
+			Main.NETWORK.sendToServer(new MessageTp(X, Y, Z));
 		}
 	}
 
