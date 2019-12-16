2d860f4a9f0cb4f337aed637b62d053f40131e09
mcDandy <danda.martinek@gmail.com>
2019-12-16 15:45:12 +0100

    changed fire charge based entity to snowball based entity

--- src/main/java/cz/mcDandy/winksmod/Entities/SunSpell.java
+++ src/main/java/cz/mcDandy/winksmod/Entities/SunSpell.java
@@ -4,6 +4,8 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.projectile.AbstractFireballEntity;
+import net.minecraft.entity.projectile.ProjectileItemEntity;
+import net.minecraft.item.Item;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.EntityRayTraceResult;
 import net.minecraft.util.math.RayTraceResult;
@@ -12,17 +14,19 @@ import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 
-public class SunSpell extends AbstractFireballEntity {
+public class SunSpell extends ProjectileItemEntity {
 	public int explosionPower = 1;
+	public Entity thrower;
+	   public SunSpell(World worldIn, LivingEntity throwerIn) {
+		      super(EntityType.SNOWBALL, throwerIn, worldIn);
+		thrower = throwerIn;   
+	   }
 
-	@OnlyIn(Dist.CLIENT)
-	public SunSpell(World worldIn, double x, double y, double z, double accelX, double accelY, double accelZ) {
-		super(EntityType.FIREBALL, x, y, z, accelX, accelY, accelZ, worldIn);
-	}
+		   public SunSpell(World worldIn, double x, double y, double z) {
+		      super(EntityType.SNOWBALL, x, y, z, worldIn);
+		  //    thrower = throwerIn;
+		   }
 
-	public SunSpell(World worldIn, LivingEntity shooter, double accelX, double accelY, double accelZ) {
-		super(EntityType.FIREBALL, shooter, accelX, accelY, accelZ, worldIn);
-	}
 
 	/**
 	 * Called when this EntityFireball hits a block or entity.
@@ -31,17 +35,22 @@ public class SunSpell extends AbstractFireballEntity {
 		if (!this.world.isRemote) {
 			if (result.getType() == RayTraceResult.Type.ENTITY) {
 				Entity entity = ((EntityRayTraceResult) result).getEntity();
-				entity.attackEntityFrom(DamageSource.causeFireballDamage(this, this.shootingEntity), 6.0F);
-				this.applyEnchantments(this.shootingEntity, entity);
+	//			entity.attackEntityFrom(DamageSource.(this, this.thrower), 6.0F);
+			//	this.applyEnchantments(this.shootingEntity, entity);
 			}
 
-			boolean flag = net.minecraftforge.event.ForgeEventFactory.getMobGriefingEvent(this.world,
-					this.shootingEntity);
+			boolean flag = net.minecraftforge.event.ForgeEventFactory.getMobGriefingEvent(this.world,this.thrower);
 			this.world.createExplosion((Entity) null, this.posX, this.posY, this.posZ, (float) this.explosionPower,
 					flag, flag ? Explosion.Mode.DESTROY : Explosion.Mode.NONE);
 			this.remove();
 		}
 
+	}
+
+	@Override
+	protected Item getDefaultItem() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
