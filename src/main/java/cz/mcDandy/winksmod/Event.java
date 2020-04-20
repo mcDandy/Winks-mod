8cd25842cc3b9d9586e67e94b8f256330ba3aa32
mcDandy <danda.martinek@gmail.com>
2020-04-20 21:10:02 +0200

    RegistryRenderer
    It needs EntityType not class

--- src/main/java/cz/mcDandy/winksmod/Event.java
+++ src/main/java/cz/mcDandy/winksmod/Event.java
@@ -1,21 +1,14 @@
 package cz.mcDandy.winksmod;
 
-import javax.annotation.Nonnull;
-
 import com.google.common.base.Preconditions;
-
 import cz.mcDandy.winksmod.Blocks.ModBlocks;
 import cz.mcDandy.winksmod.Dimensions.Biomes.ModBiomes;
-import cz.mcDandy.winksmod.Entities.SunSpell;
-import cz.mcDandy.winksmod.Utils.NoAutomaticBlockItem;
-import cz.mcDandy.winksmod.Dimensions.ModDimensions;
 import cz.mcDandy.winksmod.Entities.ModEntities;
+import cz.mcDandy.winksmod.Entities.SunSpell;
 import cz.mcDandy.winksmod.Items.ModItems;
+import cz.mcDandy.winksmod.Render.Entities.SunSpellRenderer;
+import cz.mcDandy.winksmod.Utils.NoAutomaticBlockItem;
 import net.minecraft.block.Block;
-import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.entity.EntityRenderer;
-import net.minecraft.client.renderer.entity.EntityRendererManager;
-import net.minecraft.client.renderer.entity.SpriteRenderer;
 import net.minecraft.entity.EntityType;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
@@ -23,17 +16,15 @@ import net.minecraft.item.ItemGroup;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.biome.Biome;
 import net.minecraftforge.client.event.ModelRegistryEvent;
-import net.minecraftforge.common.DimensionManager;
-import net.minecraftforge.common.ModDimension;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.client.registry.IRenderFactory;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber;
-import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.IForgeRegistry;
 import net.minecraftforge.registries.IForgeRegistryEntry;
+
+import javax.annotation.Nonnull;
 
 @EventBusSubscriber(modid = Main.MODID, bus = EventBusSubscriber.Bus.MOD)
 public class Event {
@@ -110,7 +101,7 @@ public class Event {
 
     @SubscribeEvent
     public static void setupModels(ModelRegistryEvent event) {
-
+        RenderingRegistry.registerEntityRenderingHandler(SunSpell.class, new SunSpellRenderer.Factory());
     }
 
     @Nonnull
