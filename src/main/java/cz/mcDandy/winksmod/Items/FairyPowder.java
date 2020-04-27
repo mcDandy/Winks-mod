ca66abbe87629c46fb725532f66a35e52c5d1d7d
mcDandy <danda.martinek@gmail.com>
2020-04-27 13:37:10 +0200

    Fairy powder is edible. Removes any negative potion effect.

---
+++ src/main/java/cz/mcDandy/winksmod/Items/FairyPowder.java
@@ -0,0 +1,35 @@
+package cz.mcDandy.winksmod.Items;
+
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.item.Food;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.potion.Effect;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.world.World;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class FairyPowder extends Item {
+    public FairyPowder(Item.Properties properties)  {
+        super(properties);
+    }
+    @Override
+    public ItemStack onItemUseFinish(ItemStack stack, World worldIn, LivingEntity entityLiving) 
+    {
+        ItemStack itemStack = super.onItemUseFinish(stack, worldIn, entityLiving);
+        if (!worldIn.isRemote) {
+            Collection<EffectInstance> effects = entityLiving.getActivePotionEffects();
+            Collection<EffectInstance> effects2 = new ArrayList<>();
+            for (EffectInstance e:effects) {
+                if(!e.getPotion().isBeneficial())effects2.add(e);
+            }
+            for (EffectInstance e:effects2) {
+                entityLiving.removePotionEffect(e.getPotion());
+            }
+        }
+        return itemStack;
+    }
+}
--- src/main/java/cz/mcDandy/winksmod/Items/ModItems.java
+++ src/main/java/cz/mcDandy/winksmod/Items/ModItems.java
@@ -1,10 +1,11 @@
 package cz.mcDandy.winksmod.Items;
 
+import net.minecraft.item.Food;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 
 public class ModItems {
-	public static Item FAIRY_POWDER = new Item(new Item.Properties().group(/* ModItemGroups.WMOD */ItemGroup.DECORATIONS))
+	public static Item FAIRY_POWDER = new FairyPowder(new Item.Properties().group(/* ModItemGroups.WMOD */ItemGroup.DECORATIONS).food(new Food.Builder().fastToEat().setAlwaysEdible().build()))
 			.setRegistryName("fairy_powder");
 	public static Item SOLARIA_WAND = new SolariaWand(
 			new Item.Properties().group(/* ModItemGroups.WMOD */ItemGroup.DECORATIONS)).setRegistryName("solaria_wand");
