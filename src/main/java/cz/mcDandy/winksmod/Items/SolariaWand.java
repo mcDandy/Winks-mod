31e581478449ec605493a8136afa29f89ea9ef19
mcDandy <danda.martinek@gmail.com>
2020-06-06 10:19:15 +0200

    Shooting from SolariaWand costs energy

--- src/main/java/cz/mcDandy/winksmod/Items/SolariaWand.java
+++ src/main/java/cz/mcDandy/winksmod/Items/SolariaWand.java
@@ -1,8 +1,12 @@
 package cz.mcDandy.winksmod.Items;
 
+import cz.mcDandy.winksmod.Capabilities.FairyEnergy;
+import cz.mcDandy.winksmod.Capabilities.FairyEnergyCapability;
+import cz.mcDandy.winksmod.Capabilities.IFairyEnergy;
 import cz.mcDandy.winksmod.Entities.SunSpell;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.player.ServerPlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.UseAction;
@@ -11,6 +15,8 @@ import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
 import cz.mcDandy.winksmod.Main;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.fml.LogicalSide;
 
 public class SolariaWand extends Item {
 	int speed = 1;
@@ -32,12 +38,25 @@ public class SolariaWand extends Item {
 	public void onPlayerStoppedUsing(ItemStack stack, World worldIn, LivingEntity entityLiving, int timeLeft) {
 		super.onPlayerStoppedUsing(stack, worldIn, entityLiving, timeLeft);
 		Main.LOGGER.info("Loook vector:" + entityLiving.getLookVec());
-		SunSpell s = new SunSpell(worldIn, entityLiving,0,0,0);
-		s.setPosition(entityLiving.getPosition().getX(),entityLiving.getPosition().getY()+ entityLiving.getEyeHeight(),entityLiving.getPosition().getZ());
-		 s.accelerationX = entityLiving.getLookVec().x;
-		 s.accelerationY = entityLiving.getLookVec().y;
-		 s.accelerationZ = entityLiving.getLookVec().z;
-		worldIn.addEntity(s);
+		if(!worldIn.isRemote)
+		{
+			if (entityLiving instanceof ServerPlayerEntity)
+			{
+				entityLiving.getCapability(FairyEnergyCapability.FAIRY_ENERGY_CAPABILITY).ifPresent(fe ->{
+				Main.LOGGER.info(fe.getAmount());
+				if(fe.getAmount()>1)
+				{
+					SunSpell s = new SunSpell(worldIn, entityLiving,0,0,0);
+					s.setPosition(entityLiving.getPosition().getX(),entityLiving.getPosition().getY()+ entityLiving.getEyeHeight(),entityLiving.getPosition().getZ());
+					s.accelerationX = entityLiving.getLookVec().x;
+					s.accelerationY = entityLiving.getLookVec().y;
+					s.accelerationZ = entityLiving.getLookVec().z;
+					worldIn.addEntity(s);
+					fe.addOrSubtractAmount(-1);
+				}
+			});
+			}
+		}
 	}
 
 	@Override
